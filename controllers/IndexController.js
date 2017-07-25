82853862625336f68ec127b25a33c32b6869d2cb
Alrik Zachert <az@kreatiefer.de>
2017-07-25 19:31:53 +0200

    Load the latest 6 release notes to the start page.

--- controllers/IndexController.js
+++ controllers/IndexController.js
@@ -1,46 +1,26 @@
 'use strict';
 
 const AbstractController = require('./AbstractController');
-const ReleaseNotesLoader = require('@release-notes/node/lib/ReleaseNotesLoader');
-const multer = require('multer');
-
-const releaseNotesLoader = new ReleaseNotesLoader();
-const uploadHandler = multer();
 
 class IndexController extends AbstractController {
   indexAction(req, res, next) {
-    res.render('index/index', {});
-  }
+    this.serviceManager.get('releaseNotesRepository').findNewest(
+      6, (err, releaseNotesList) => {
+        if (err) {
+          return void next(err);
+        }
 
-  showReleaseNotesAction(req, res, next) {
-    if (!req.file) {
-      return void res.render('index/index', {
-        err: new Error('No release-notes.yml file was uploaded.')
-      });
-    }
-
-    releaseNotesLoader.loadReleaseNotes(req.file.buffer, (err, releaseNotes) => {
-      if (err) {
-        res.statusCode = 400;
+        res.render('index/index', {
+          newest: releaseNotesList,
+        });
       }
-
-      res.render('index/index', {
-        err,
-        releaseNotes,
-      })
-    });
+    );
   }
 
   getRoutes() {
     return {
       '/': [{
         handler: (req, res, next) => this.indexAction(req, res, next)
-      }, {
-        method: 'post',
-        handler: [
-          uploadHandler.single('release-notes'),
-          (req, res, next) => this.showReleaseNotesAction(req, res, next),
-        ]
       }],
     }
   };
