70ce9cfe4a0192ad766eac75f6fd51d32cd43024
Alrik Zachert <az@kreatiefer.de>
2017-08-26 01:14:23 +0200

    Add support for local config overrides

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 'use strict';
 
 const WebApp = require('./services/WebApp');
+
 const app = new WebApp();
 
 app
   .configure({
     files: [
-      __dirname + '/config/application.js'
-    ]
+      `${__dirname}/config/application.js`,
+      `${__dirname}/config/application.local.js`,
+    ],
   })
   .bootstrap()
   .launch();
