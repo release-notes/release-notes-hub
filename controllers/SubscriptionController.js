d734e57b1fbeaacb48488fd968b390d4c2aa54d0
Alrik Zachert <az@kreatiefer.de>
2017-09-19 17:45:35 +0200

    Diaplay all subscriptions of an authenticated user.

--- controllers/SubscriptionController.js
+++ controllers/SubscriptionController.js
@@ -3,8 +3,42 @@
 const AbstractController = require('./AbstractController');
 
 class SubscriptionController extends AbstractController {
-  static renderSubscriptionsView(req, res, next) {
-    res.render('subscriptions/index');
+  /**
+   * @property {SubscriptionRepository} subscriptionRepository
+   */
+
+  bootstrap() {
+    super.bootstrap();
+
+    const sm = this.getServiceManager();
+
+    this.subscriptionRepository = sm.get('subscriptionRepository', true);
+    this.releaseNotesRepository = sm.get('releaseNotesRepository', true);
+
+    return this;
+  }
+
+  renderSubscriptionsView(req, res, next) {
+    this.subscriptionRepository.findBySubscriberId(
+      req.user._id,
+      (lookupErr, subscriptions) => {
+        if (lookupErr) return void next(lookupErr);
+
+        res.render('subscriptions/index', {
+          subscriptions,
+        });
+      }
+    );
+  }
+
+  loadReleaseNotes(scope, name, callback) {
+    this.releaseNotesRepository.findOneByScopeAndName(
+      scope,
+      name,
+      callback
+    );
+
+    return this;
   }
 
   getRoutes() {
@@ -16,9 +50,9 @@ class SubscriptionController extends AbstractController {
         handler: [
           authService.authenticate('session'),
           authService.requireUser(),
-          (req, res, next) => SubscriptionController.renderSubscriptionsView(req, res, next)
-        ]
-      }]
+          (req, res, next) => this.renderSubscriptionsView(req, res, next)
+        ],
+      }],
     };
   }
 }
