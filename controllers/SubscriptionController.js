1a6f8ac094261e91feddd1b7ae3178ccb964e802
Alrik Zachert <az@kreatiefer.de>
2017-09-19 17:46:55 +0200

    Add support for subscribing to release notes-

--- controllers/SubscriptionController.js
+++ controllers/SubscriptionController.js
@@ -5,6 +5,7 @@ const AbstractController = require('./AbstractController');
 class SubscriptionController extends AbstractController {
   /**
    * @property {SubscriptionRepository} subscriptionRepository
+   * @property {ReleaseNotesRepository} releaseNotesRepository
    */
 
   bootstrap() {
@@ -31,6 +32,57 @@ class SubscriptionController extends AbstractController {
     );
   }
 
+  renderSubscribeToRealeaseNotesView(req, res, next) {
+    const scope = req.params.scope;
+
+    this.loadReleaseNotes(
+      scope,
+      req.params.releaseNotesId,
+      (err, releaseNotesModel) => {
+        if (err) {
+          return void next(err);
+        }
+
+        // not found
+        if (!releaseNotesModel) {
+          return void next();
+        }
+
+        res.render('subscriptions/subscribe', {
+          releaseNotesModel,
+          scope,
+        });
+      }
+    );
+  }
+
+  subscripeToRealeaseNotes(req, res, next) {
+    const scope = req.params.scope;
+    const releaseNotesId = req.params.releaseNotesId;
+
+    this.loadReleaseNotes(
+      scope,
+      releaseNotesId,
+      (lookupErr, releaseNotesModel) => {
+        if (lookupErr) return void next(lookupErr);
+
+        // not found
+        if (!releaseNotesModel) {
+          return void next();
+        }
+
+        this.subscriptionRepository.create({
+          subscriberId: req.user._id,
+          releaseNotesId: releaseNotesModel._id,
+        }, (persistencyErr/*, subscription*/) => {
+          if (persistencyErr) return void next(persistencyErr);
+
+          res.redirect('/subscriptions');
+        });
+      }
+    );
+  }
+
   loadReleaseNotes(scope, name, callback) {
     this.releaseNotesRepository.findOneByScopeAndName(
       scope,
@@ -51,8 +103,23 @@ class SubscriptionController extends AbstractController {
           authService.authenticate('session'),
           authService.requireUser(),
           (req, res, next) => this.renderSubscriptionsView(req, res, next)
-        ],
+        ]
       }],
+      '/@:scope/:releaseNotesId/subscribe': [{
+        method: 'get',
+        handler: [
+          authService.authenticate('session'),
+          authService.requireUser(),
+          (req, res, next) => this.renderSubscribeToRealeaseNotesView(req, res, next),
+        ]
+      }, {
+        method: 'post',
+        handler: [
+          authService.authenticate('session'),
+          authService.requireUser(),
+          (req, res, next) => this.subscripeToRealeaseNotes(req, res, next),
+        ]
+      }]
     };
   }
 }
